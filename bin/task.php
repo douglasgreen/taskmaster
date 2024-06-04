18be6b609ecc9e0b15523f8b66e6fb2bbe247345
Douglas S. Green <douglas@nurd.site>
2024-06-04 18:10:45 -0400

    refactor: combining two commands

---
+++ bin/task.php
@@ -0,0 +1,78 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+use DouglasGreen\OptParser\OptParser;
+use DouglasGreen\TaskMaster\ReminderEmail;
+use DouglasGreen\TaskMaster\TaskFile;
+use DouglasGreen\TaskMaster\TaskProcessor;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$optParser = new OptParser('Task Manager', 'Command-line version of task manager');
+
+// Add commands.
+$optParser->addCommand(['process'], 'Process tasks and send emails')
+    ->addCommand(['add'], 'Add a new task');
+
+// Add params for process command.
+$optParser->addTerm('email', 'EMAIL', 'Your email address')
+    ->addParam(['timezone', 't'], 'STRING', 'Your timezone');
+
+// Add params for add command.
+$optParser->addTerm('name', 'STRING', 'Task name')
+    ->addParam(['url'], 'URL', 'URL for documentation or action')
+    ->addParam(['recur'], 'BOOL', 'Recurring?')
+    ->addParam(['start'], 'DATE', 'Recur start date')
+    ->addParam(['end'], 'DATE', 'Recur end date')
+    ->addParam(['year'], 'STRING', 'Days of year')
+    ->addParam(['month'], 'STRING', 'Days of month')
+    ->addParam(['week'], 'STRING', 'Days of week')
+    ->addParam(['time'], 'STRING', 'Times of day');
+
+// Add usage for process command.
+$optParser->addUsage(['process', 'email', 'timezone']);
+
+// Add usage for add command.
+$optParser->addUsage(['add', 'name', 'url', 'recur', 'start', 'end', 'year', 'month', 'week', 'time']);
+
+$input = $optParser->parse();
+
+$command = $input->getCommand();
+
+$filename = __DIR__ . '/../assets/data/tasks.csv';
+
+switch ($command) {
+    case 'process':
+        $timezone = $input->get('timezone');
+        if ($timezone !== null) {
+            date_default_timezone_set((string) $timezone);
+        }
+
+        $email = $input->get('email');
+        if ($email === null) {
+            die('Email is required' . PHP_EOL);
+        }
+
+        $reminderEmail = new ReminderEmail((string) $email);
+        $taskFile = new TaskFile($filename);
+        $taskProcessor = new TaskProcessor($reminderEmail, $taskFile);
+        $taskProcessor->processTasks();
+        break;
+
+    case 'add':
+        $taskFile = new TaskFile($filename);
+        $taskFile->addTask(
+            (string) $input->get('name'),
+            (string) $input->get('url'),
+            (bool) $input->get('recur'),
+            (string) $input->get('start'),
+            (string) $input->get('end'),
+            (string) $input->get('year'),
+            (string) $input->get('month'),
+            (string) $input->get('week'),
+            (string) $input->get('time')
+        );
+        break;
+}
